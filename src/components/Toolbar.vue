8d98ce06dc119c84f057ad082b31346010e29043
lohith <viperlohith121@gmail.com>
2019-05-13 12:12:51 +0530

    'changes'

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,10 @@
+<template>
+        <v-toolbar app height="60" >
+            <v-toolbar-title class="headline text-uppercase">
+                <span>Lohith</span>
+                <span class="font-weight-light">Cj</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+                <router-link to="/contact"><v-btn>contact</v-btn></router-link>
+         </v-toolbar>
+</template>
